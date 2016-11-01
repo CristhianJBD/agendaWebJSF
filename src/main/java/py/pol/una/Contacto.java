e74960e721e2389f08ba7da7b9208c113f040399
Cristhian <cristhian.benitez5@gmail.com>
2016-10-31 23:58:33 -0300

    clases creadas
    Signed-off-by: Cristhian <cristhian.benitez5@gmail.com>

---
+++ src/main/java/py/pol/una/Contacto.java
@@ -0,0 +1,61 @@
+package py.pol.una;
+
+import java.util.Date;
+
+public class Contacto {
+
+	private String nombre;
+	private String apellido;
+	private String alias;
+	private String email;
+	private String direccion;
+	private Date fechaCreacion;
+	private Date fechaModificacion;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public Date getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setFechaModificacion(Date fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	
+	
+}
---
+++ src/main/java/py/pol/una/ContactosFormMB.java
@@ -0,0 +1,10 @@
+package py.pol.una;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ContactosFormMB {
+
+}
