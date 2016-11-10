f188074ebf5bede72d627b92d6a9069463f2f9aa
Crithian Benitez <kvera@pol.una.py>
2016-11-10 11:20:02 -0300

    vista
    Signed-off-by: Crithian Benitez <kvera@pol.una.py>

--- src/main/java/py/pol/una/ContactosFormMB.java
+++ src/main/java/py/pol/una/ContactosFormMB.java
@@ -1,34 +1,77 @@
 package py.pol.una;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
+import org.omg.CORBA.portable.OutputStream;
 
-@ManagedBean
-@SessionScoped
+@ManagedBean(name = "FormController", eager = true)
+@RequestScoped
 public class ContactosFormMB {
 
-	private Contacto contacto = new Contacto();
-	
-	
+    private Contacto contacto = new Contacto();
 
+    public void agregarContacto() {
 
-	public void agregarContacto(){
-		
-	}
-	
-	public void limpiar(){
-		
-	}
-	
-	
-	
-	public Contacto getContacto() {
-		return contacto;
-	}
+        try {
 
+            URL url = new URL("https://desa03.konecta.com.py/pwf/rest/agenda");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
 
-	public void setContacto(Contacto contacto) {
-		this.contacto = contacto;
-	}
+            String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
+
+            OutputStream os = (OutputStream) conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void limpiar() {
+
+    }
+
+    public Contacto getContacto() {
+
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+
+        this.contacto = contacto;
+    }
 
 }
--- src/main/java/py/pol/una/ContactosListadoMB.java
+++ src/main/java/py/pol/una/ContactosListadoMB.java
@@ -1,38 +1,94 @@
 package py.pol.una;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+@ManagedBean(name = "listaController", eager = true)
+@SessionScoped
 public class ContactosListadoMB {
 
-	private List<Contacto> listaContactos = new ArrayList<>();
+    private List<Contacto> contactos = new ArrayList<>();
 
-	
-	public void editarContacto(){
-		
-	}
-	
-	public void eliminarContacto(){
-		
-	}
-	
-	public void verContacto(){
-		
-	}
-	
-	public void listaContactos(){
-		
-	}
-	
-	
-	
-	
-	public List<Contacto> getListaContactos() {
-		return listaContactos;
-	}
+    @PostConstruct
+    public void init() {
 
-	public void setListaContactos(List<Contacto> listaContactos) {
-		this.listaContactos = listaContactos;
-	}
-	
+        this.contactos = new ArrayList<Contacto>();
+        setContactos(listaContactos());
+    }
+
+    public void editarContacto() {
+
+    }
+
+    public void eliminarContacto() {
+
+    }
+
+    public void verContacto() {
+
+    }
+
+    public List<Contacto> listaContactos() {
+
+        try {
+
+            URL url = new URL("https://desa03.konecta.com.py/pwf/rest/agenda");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                Gson gson = new GsonBuilder().create();
+                Contacto lista = gson.fromJson(output, null);
+
+            }
+
+            conn.disconnect();
+
+            return null;
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    public List<Contacto> getContactos() {
+
+        return contactos;
+    }
+
+    public void setContactos(List<Contacto> contactos) {
+
+        this.contactos = contactos;
+    }
+
 }
